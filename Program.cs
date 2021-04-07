e7410c36d1dda438a31b7477344bdc0d337af776
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-07 10:11:06 +0300

    better Solution mayBe

--- Program.cs
+++ Program.cs
@@ -7,57 +7,33 @@ namespace TrafficJam
     {
         static void Main(string[] args)
         {
-            int countCarsAllowedToPass = int.Parse(Console.ReadLine());
-
+            int carPassOnGreen = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
             Queue<string> cars = new Queue<string>();
-
             int countPassed = 0;
-            int count = 0;
 
-            while (true)
-            {                
-                string line = Console.ReadLine();
-
-                if (line == "end")
+            while (command != "end")
+            {
+                if (command == "green")
                 {
-                    Console.WriteLine($"{countPassed} cars passed the crossroads.");
-                    //ToDo
-                    break;
-                }
-
-                if (line == "green")
-                {
-                    if (count <= countCarsAllowedToPass)
-                    {      
-                        for (int i = 0; i < count; i++)
-                        {
-                            string car = cars.Dequeue();
-                            Console.WriteLine($"{car} passed!");                            
-                        }
-
-                        countPassed += count;
-                        count -= count;
-
-                    }
-                    else // if count > countCarsAllowedToPass
+                    for (int i = 0; i < carPassOnGreen; i++)
                     {
-                        countPassed += countCarsAllowedToPass;
-
-                        for (int i = 0; i < countCarsAllowedToPass; i++)
+                        if (cars.Count > 0)
                         {
-                            string car = cars.Dequeue();
-                            Console.WriteLine($"{car} passed!");                            
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            countPassed++;
                         }
-
-                        count -= countCarsAllowedToPass;
                     }
                 }
-                else // line = some car to enter
+                else 
                 {
-                    cars.Enqueue(line);
-                    count++;
+                    cars.Enqueue(command);
                 }
+
+                command = Console.ReadLine();
             }
+
+            Console.WriteLine($"{countPassed} cars passed the crossroads.");
         }
     }
 }
